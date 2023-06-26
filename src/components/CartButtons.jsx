05fee7c4c3a9bf20236fccf8a605575605977838
Anil Acar <ostravba@gmail.com>
2023-06-26 23:07:53 +0200

    cart button done without styling

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+import { FaUserCheck, FaUserTimes, FaShoppingCart } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const CartButtons = () => {
-  return <h4 className="cart-btn-wrapper">Cart Buttons</h4>
+  return (
+    <Wrapper className="cart-btn-wrapper">
+      <Link to="/cart" className="cart-btn">
+        Cart
+        <span className="cart-container">
+          <FaShoppingCart />
+          <span className="cart-value">10</span>
+        </span>
+      </Link>
+      <button type="button" className="auth-btn">
+        Login <FaUserCheck />
+      </button>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  width: 12rem;
+
+  .cart-btn {
+    color: var(--red-999);
+    font-size: 1.2rem;
+    letter-spacing: var(--spacing);
+    display: flex;
+    align-items: center;
+  }
+`
 
 export default CartButtons
